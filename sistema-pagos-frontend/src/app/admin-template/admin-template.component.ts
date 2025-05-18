26f3d63df73e399c8c429e25a660218e443f14f6
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-05-18 20:02:42 +0000

    drawer added

--- sistema-pagos-frontend/src/app/admin-template/admin-template.component.ts
+++ sistema-pagos-frontend/src/app/admin-template/admin-template.component.ts
@@ -3,11 +3,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-admin-template',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule],
   templateUrl: './admin-template.component.html',
   styleUrl: './admin-template.component.css'
 })
