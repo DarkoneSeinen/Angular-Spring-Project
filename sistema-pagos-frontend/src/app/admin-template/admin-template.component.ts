3d75c5eb7d48f0ff15dcdd29e56ec548b621fcdc
Jhon-Doe10 <andrewapontebustos@gmail.com>
2025-05-29 01:44:11 +0000

    mat-drawer added

--- sistema-pagos-frontend/src/app/admin-template/admin-template.component.ts
+++ sistema-pagos-frontend/src/app/admin-template/admin-template.component.ts
@@ -4,11 +4,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-admin-template',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, MatListModule],
   templateUrl: './admin-template.component.html',
   styleUrl: './admin-template.component.css'
 })
