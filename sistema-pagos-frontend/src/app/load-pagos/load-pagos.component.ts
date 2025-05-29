0cb5a0cb625654fb6c69ad100cb268ba18f16852
Jhon-Doe10 <andrewapontebustos@gmail.com>
2025-05-29 01:46:38 +0000

    new components

---
+++ sistema-pagos-frontend/src/app/load-pagos/load-pagos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-pagos',
+  imports: [],
+  templateUrl: './load-pagos.component.html',
+  styleUrl: './load-pagos.component.css'
+})
+export class LoadPagosComponent {
+
+}
