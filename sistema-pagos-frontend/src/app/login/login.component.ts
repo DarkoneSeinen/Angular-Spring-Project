3bb603bdd4c928f931d91a268df3ce8877c3dd68
Jhon-Doe10 <andrewapontebustos@gmail.com>
2025-05-31 02:06:03 +0000

    feat: enhance login component with improved styles and validation

--- sistema-pagos-frontend/src/app/login/login.component.ts
+++ sistema-pagos-frontend/src/app/login/login.component.ts
@@ -1,35 +1,54 @@
 import { AuthService } from '../services/auth.service';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule, 
+    MatDividerModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   public loginForm: FormGroup;
+  hidePassword = true;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
-    this.loginForm = this.formBuilder.group({ // el form builder se encarga de crear el formulario reactivo
-      username: this.formBuilder.control(''),
-      password: this.formBuilder.control('')
+  constructor(
+    private formBuilder: FormBuilder, 
+    private authService: AuthService, 
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
   login(): void {
-    let username = this.loginForm.value.username;
-    let password = this.loginForm.value.password;
-    let auth: boolean = this.authService.login(username, password);
-    if (auth) {
-      this.router.navigateByUrl('/admin');
+    if (this.loginForm.valid) {
+      const username = this.loginForm.value.username;
+      const password = this.loginForm.value.password;
+      const auth: boolean = this.authService.login(username, password);
+      if (auth) {
+        this.router.navigateByUrl('/admin');
+      }
     }
   }
 }
