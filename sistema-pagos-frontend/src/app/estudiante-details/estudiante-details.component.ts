a4d31cbaf936326ab25f7bb50e909f5ed24ccb39
Jhon-Doe10 <andrewapontebustos@gmail.com>
2025-05-31 01:20:44 +0000

    style: enhance payment button design and update component imports

--- sistema-pagos-frontend/src/app/estudiante-details/estudiante-details.component.ts
+++ sistema-pagos-frontend/src/app/estudiante-details/estudiante-details.component.ts
@@ -7,13 +7,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @Component({
   selector: 'app-estudiante-details',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatCardModule, MatDivider, MatTableModule],
+  imports: [
+    CommonModule, 
+    MatToolbarModule, 
+    MatCardModule, 
+    MatDivider, 
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './estudiante-details.component.html',
   styleUrl: './estudiante-details.component.css'
 })
